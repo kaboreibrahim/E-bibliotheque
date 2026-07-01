69122a6c1918075a89a347db296f2b55c2810dc7
kaboreibrahim <147706870+kaboreibrahim@users.noreply.github.com>
2026-07-01 09:09:07 +0000

    first commit

--- core/settings.py
+++ core/settings.py
@@ -262,7 +262,7 @@ DATABASES = {
         'PORT': '3306',
         'OPTIONS': {
             'charset': 'utf8mb4',
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'; SET time_zone='+00:00'",
         },
         'CONN_MAX_AGE': 60,
     }
@@ -331,7 +331,7 @@ MESSAGE_TAGS = {
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
 LANGUAGE_CODE = 'fr-fr'
-TIME_ZONE = 'Africa/Abidjan'
+TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
 
