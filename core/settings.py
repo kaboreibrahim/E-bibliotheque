37e0d2ef586c2be7cc56065f7f2b3aa0433e621a
kaboreibrahim <147706870+kaboreibrahim@users.noreply.github.com>
2026-09-21 15:14:43 +0000

    deploiment de la V1

--- core/settings.py
+++ core/settings.py
@@ -239,38 +239,38 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 # Pour PostgreSQL (optionnel)
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT'),
-
-        'OPTIONS': {
-            'connect_timeout': 10,
-        },
-        'CONN_MAX_AGE': 60,   # connexions persistantes (60 sec)
-    }
-}
-
-# # Pour MySQL (actif)
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'c2745157c_BIBLIOv1',
-#         'USER': 'c2745157c_user',
-#         'PASSWORD': '#4h].2WrwNrELChB',
-#         'HOST': 'localhost',      # ou l'adresse de ton serveur MySQL
-#         'PORT': '3306',
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': config('DB_NAME'),
+#         'USER': config('DB_USER'),
+#         'PASSWORD': config('DB_PASSWORD'),
+#         'HOST': config('DB_HOST'),
+#         'PORT': config('DB_PORT'),
+
 #         'OPTIONS': {
-#             'charset': 'utf8mb4',
-#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES', time_zone='+00:00'",
+#             'connect_timeout': 10,
 #         },
-#         'CONN_MAX_AGE': 60,
+#         'CONN_MAX_AGE': 60,   # connexions persistantes (60 sec)
 #     }
 # }
+
+# Pour MySQL (actif)
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'c2745157c_BIBLIOv1',
+        'USER': 'c2745157c_user',
+        'PASSWORD': '#4h].2WrwNrELChB',
+        'HOST': 'localhost',      # ou l'adresse de ton serveur MySQL
+        'PORT': '3306',
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES', time_zone='+00:00'",
+        },
+        'CONN_MAX_AGE': 60,
+    }
+}
 
 
 # =============================================================================
